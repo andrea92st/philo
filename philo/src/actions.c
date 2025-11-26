bc650a9b9581906bfd7a56c47a462242c460e55d
Andrea Fiorito <anfiorit@f2r10s8.clusters.42paris.fr>
2025-11-26 15:16:10 +0100

    pls

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: anfiorit <anfiorit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 13:33:00 by fio               #+#    #+#             */
-/*   Updated: 2025/11/25 19:47:00 by anfiorit         ###   ########.fr       */
+/*   Updated: 2025/11/26 15:15:44 by anfiorit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,21 @@
 
 void	philo_eat(t_philo *philo)
 {
+	if (philo->data->nb_philos == 1)
+	{
+		pthread_mutex_lock(&philo->data->forks[philo->left_fork]);
+		print_status(philo, "has taken a fork");
+		pthread_mutex_unlock(&philo->data->forks[philo->left_fork]);
+		return ;
+	}
 	pthread_mutex_lock(&philo->data->forks[philo->left_fork]);
 	print_status(philo, "has taken a fork");
 	pthread_mutex_lock(&philo->data->forks[philo->right_fork]);
 	print_status(philo, "has taken a fork");
+	print_status(philo, "is eating");
 	pthread_mutex_lock(&philo->data->stop_mutex);
 	philo->last_meal = get_time();
 	pthread_mutex_unlock(&philo->data->stop_mutex);
-	print_status(philo, "is eating");
 	philo->meals_eaten++;
 	usleep(philo->data->time_to_eat * 1000);
 	pthread_mutex_unlock(&philo->data->forks[philo->right_fork]);
